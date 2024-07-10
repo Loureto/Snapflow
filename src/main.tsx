2381900f06ed3c79ba1e24c4753f24e922d0a736
joao <loureto.tads123@gmail.com>
2024-07-10 18:00:11 -0400

    feat: test

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { TansTackQueryProvider } from '@/providers'
 import App from './app.tsx'
 import './globals.css'
 
+const test = 'test'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TansTackQueryProvider>
     <App />
